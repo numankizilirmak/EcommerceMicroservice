c281b12b74897f83530ae334c9e0470ffbfd491c
Numan KIZILIRMAK <nmnkizilirmak@gmail.com>
2021-06-06 16:18:49 +0300

    CatalogController test

--- src/Catalog/Catalog.Api/Controllers/CatalogController.cs
+++ src/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Catalog.Api.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class CatalogController: ControllerBase
     {
+        [HttpGet]
+        public string Get()
+        {
+            return "api is working v1";
+        }
     }
 }
