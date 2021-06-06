3331a497d7c28025db51203fc7587a3fbc91d512
Numan KIZILIRMAK <nmnkizilirmak@gmail.com>
2021-06-06 16:16:13 +0300

    v13

--- EcommerceGitActionTest/Controllers/WeatherForecastController.cs
+++ EcommerceGitActionTest/Controllers/WeatherForecastController.cs
@@ -22,7 +22,7 @@ namespace EcommerceGitActionTest.Controllers
         [HttpGet]
         public string Get()
         {
-            return "api is working v12";
+            return "api is working v13";
         }
     }
 }
