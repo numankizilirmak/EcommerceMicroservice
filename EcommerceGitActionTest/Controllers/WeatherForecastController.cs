89a71362d20b80675c13b0d55dbb309ca9fa92fa
Numan KIZILIRMAK <nmnkizilirmak@gmail.com>
2021-06-06 13:45:36 +0300

    azure git action test

--- EcommerceGitActionTest/Controllers/WeatherForecastController.cs
+++ EcommerceGitActionTest/Controllers/WeatherForecastController.cs
@@ -22,7 +22,7 @@ namespace EcommerceGitActionTest.Controllers
         [HttpGet]
         public string Get()
         {
-            return "api is working v1";
+            return "api is working v2";
         }
     }
 }
